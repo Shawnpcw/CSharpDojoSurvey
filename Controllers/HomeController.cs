ee66d5612e5e12bd80c17f8a66bd9751715444a3
Shawnpcw <s.cannon.williams@gmail.com>
2019-04-07 19:23:41 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dojo_survey.Models;
+
+namespace dojo_survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("home")]
+        public ViewResult Index()
+        {
+            return View("index");
+        }
+        [HttpPost("results")]
+        public ViewResult Results(string name, string location, string comment,string stack){
+            ViewBag.name = name;
+            ViewBag.location = location;
+            ViewBag.comment = comment;
+            ViewBag.stack = stack;            
+            return View("results");
+        } 
+
+        [HttpGet("time")]
+        public DateTime = Cu
+        // [HttpPost("postingdata")]
+        // public IActionResult postingdata(string name, string location, string comment, string stack){
+        //     return RedirectToAction("results");
+        // }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
